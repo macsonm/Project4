4b233bdd7fc017fb25eb5472b837d05b760ab06c
macsonm <75022784+macsonm@users.noreply.github.com>
2022-06-20 14:52:42 +0300

    Challenge
    1. If users try to visit a URL that isn’t allowed, show an alert saying it’s blocked.
    2. fix view = webView

--- Project4/ViewController.swift
+++ Project4/ViewController.swift
@@ -5,6 +5,7 @@ class ViewController: UIViewController {
     
     var webView: WKWebView!
     var progressView: UIProgressView!
+    var websites = ["google.com", "apple.com", "ya.ru"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -14,7 +15,7 @@ class ViewController: UIViewController {
     }
     
     func openYaUrl() {
-        let url = URL(string: "https://ya.ru")!
+        let url = URL(string: "https://" + websites[0])!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
     }
@@ -25,8 +26,9 @@ class ViewController: UIViewController {
     
     @objc func openTapped() {
         let ac = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
-        ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
-        ac.addAction(UIAlertAction(title: "ya.ru", style: .default, handler: openPage))
+        for website in websites {
+            ac.addAction(UIAlertAction(title: website, style: .default, handler: openPage))
+        }
         ac.addAction(UIAlertAction(title: "cancel", style: .cancel, handler: openPage))
         
         ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
@@ -61,9 +63,36 @@ class ViewController: UIViewController {
         //заполняемый индикатор прогресса в нижнем тулбаре
         webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
         
-        toolbarItems = [progressButton, spacer, refresh ]
+        //возврат к предыдущей странице
+        let back = UIBarButtonItem(barButtonSystemItem: .rewind, target: webView, action: #selector(webView.goBack))
+        let forwardd = UIBarButtonItem(barButtonSystemItem: .fastForward, target: webView, action: #selector(webView.goForward))
+        
+        toolbarItems = [back, forwardd, spacer, progressButton, spacer, refresh ]
         navigationController?.isToolbarHidden = false
     }
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        let url = navigationAction.request.url
+
+        if let host = url?.host {
+            for website in websites {
+                if host.contains(website) {
+                    decisionHandler(.allow)
+                    return
+                }
+            }
+        } else {
+            //TODO: алерт с уведомлением
+//            alertForbidden()
+            print("forbidden")
+        }
+        decisionHandler(.cancel)
+    }
+    
+//    func alertForbidden() {
+//        let ac = UIAlertController(title: "decidePolicy", message: "forbidden", preferredStyle: .alert)
+//        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+//    }
 }
 
 extension ViewController: WKNavigationDelegate{
@@ -71,6 +100,5 @@ extension ViewController: WKNavigationDelegate{
         webView = WKWebView()
         webView.navigationDelegate = self
         view = webView
-        
     }
 }
